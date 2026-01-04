d28afef8b10c4327533b51f74dfac9f6a460074c
Zarax34 <ahmedalslwy34@gmail.com>
2026-01-05 00:35:52 +0300

    Update Firebase configuration to new format

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,29 @@
-// Firebase Configuration
-// قم بتعديل هذه القيم ببيانات مشروع Firebase الخاص بك
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
-
-export default firebaseConfig;
+{
+  "project_info": {
+    "project_number": "540994983454",
+    "project_id": "daily-tasks-monitor",
+    "storage_bucket": "daily-tasks-monitor.firebasestorage.app"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:540994983454:android:d3b6a600cde8307512000f",
+        "android_client_info": {
+          "package_name": "com.masash.daily.tascks"
+        }
+      },
+      "oauth_client": [],
+      "api_key": [
+        {
+          "current_key": "AIzaSyCIo8MXNgxvDUhRG17Xo24dHj32fg3bVBU"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": []
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}
